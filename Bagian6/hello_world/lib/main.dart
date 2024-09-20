d96f45f320df08c565e11dfc51b8e975ed2e09f0
avislamongan@gmail.com <avislamongan@gmail.com>
2024-09-20 08:40:50 +0700

    feat: bagian 6 - add p123

--- Bagian6/hello_world/lib/main.dart
+++ Bagian6/hello_world/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widgets/fab_widget.dart';
+import 'package:hello_world/basic_widgets/image_widget.dart';
+import 'package:hello_world/basic_widgets/loading_cupertino.dart';
+import 'package:hello_world/basic_widgets/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +32,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 128, 140)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 0, 128, 140)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -105,9 +110,11 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const MyImageWidget(),
+            const MyTextWidget(),
+            // const LoadingCupertino(),
+            // const FabWidget(),
+            const Text('Avicenna Mumtaza'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
